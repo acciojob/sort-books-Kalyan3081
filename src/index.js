a73c233fdf738f9acdc03c81d4cedbb3214ef304
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-18 12:08:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import store from "./components/Store"; // Assuming this is your Redux store
+import { Provider } from "react-redux"; // Import Provider from react-redux
 
-
-
+// Render the app with Redux Provider
 ReactDOM.render(
   <Provider store={store}>
     <App />
