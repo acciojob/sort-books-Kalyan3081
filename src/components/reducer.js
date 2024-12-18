cad4e8e19fb45e9558c71fee42c5183bbf473ca0
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-18 11:52:11 +0530

    Update reducer.js

--- src/components/reducer.js
+++ src/components/reducer.js
@@ -1,22 +1,17 @@
-import { FETCH_BOOKS_LOADING, FETCH_BOOKS_SUCCESS, FETCH_BOOKS_FAILURE } from "./actions"
-
-const intialState = {
-    loading: false,
+ 
+import { createSlice } from "@reduxjs/toolkit";
+ 
+const booksSlice = createSlice({
+  name: "books",
+  initialState: {
     books: [],
-    error: null,
-}
-
-const reducer = (state = intialState, action) => {
-    switch (action.type) {
-        case 'FETCH_BOOKS_LOADING':
-            return { ...state, loading: true, books: [], error: null }
-        case 'FETCH_BOOKS_SUCCESS':
-            return { ...state, loading: false, books: action.payload, error: null }
-        case 'FETCH_BOOKS_FAILURE':
-            return { ...state, loading: false, books: [], error: action.payload }
-        default:
-            return state
-    }
-}
-
-export default reducer
+  },
+  reducers: {
+    setBooks: (state, action) => {
+      state.books = [...action.payload];
+    },
+  },
+});
+ 
+export const { setBooks } = booksSlice.actions;
+export default booksSlice.reducer;
